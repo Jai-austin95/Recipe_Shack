42b1bc5f3fa908c0c712972ab4462792db3e9f5b
Jai-austin95 <jai.austin95@gmail.com>
2020-06-01 08:41:08 +0000

    fixed page loading up with 404 error by leaving the app route as a slash for the home page.

--- app.py
+++ app.py
@@ -1,16 +1,10 @@
 from flask import Flask, redirect, url_for, render_template
 
-from flask_pymongo import PyMongo
-
-import json
-
 app = Flask(__name__)
-
-app.config["MONGO_DBNAME"] = "name here"
-app.config["MONGO_URI"] = "MONGO_URL"
-
-mongo = PyMongo(app)
 
 @app.route("/")
 def home():
     return render_template("home.html")
+
+if __name__ == "__main__":
+    app.run()
--- python-mongo/my_script.py
+++ python-mongo/my_script.py
@@ -1,1 +1,9 @@
-from pymongo import MongoClient
+from flask_pymongo import PyMongo
+import json
+
+app = Flask(__name__)
+
+app.config["MONGO_DBNAME"] = "name here"
+app.config["MONGO_URI"] = "MONGO_URL"
+
+mongo = PyMongo(app)
