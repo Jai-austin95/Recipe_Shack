cd7b0abc50ad1d7c104648ebdb372c708efd66a6
Jai-austin95 <jai.austin95@gmail.com>
2020-06-11 13:30:31 +0000

    fixed issue with edit form, changed button name on edit form and on add form

--- app.py
+++ app.py
@@ -66,7 +66,7 @@ def insert_recipe():
 
 @app.route("/edit_recipe/<recipe_id>")
 def edit_recipe(recipe_id):
-    edit_recipe = mongo.db.recipes.find_one({"_id": ObjectId()})
+    edit_recipe = mongo.db.recipes.find_one({"_id": ObjectId(recipe_id)})
     categories = list(mongo.db.Categories.find())
     return render_template("edit_recipe.html", edit=edit_recipe, categories = categories)
 
